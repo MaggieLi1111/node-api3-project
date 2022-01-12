d6b0bf040615ae31dd41dac39a8d8ddc528157da
unknown <lidanyang1111@gmail.com>
2022-01-12 13:24:04 -0800

    version1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it
+const server = require("./api/server")
+
+const express = require("express")
+
+const Port = 9000;
+
+server.listen(Port, () => {
+    console.log(`Listening on ${Port}`)
+})
